f611f1d474751e3d10cd5ee1e90acdf4f89614c7
Joseph D. Marhee <jmarhee@interiorae.com>
2021-10-28 15:35:12 -0500

    Updates kubeconfig.tf, adds scripts/retrieve_kubeconfig.sh, uses external data, rather than null_resource and redirect, to pull and repopulate kubeconfig for created cluster to allow the config itself to be in retrievable in state as formatted base64, as well as easier clean-up on destroy rather than a destroy-time local-exec.
    Signed-off-by: Joseph D. Marhee <jmarhee@interiorae.com>

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -1,18 +1,9 @@
-resource "null_resource" "cluster" {
-  depends_on = [digitalocean_database_cluster.rancherdb, digitalocean_droplet.controller-init, digitalocean_droplet.controller-peer]
-
-  provisioner "local-exec" {
-    command = "sleep 1; /usr/bin/ssh -i ${pathexpand(format("%s", local.ssh_key_name))} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q root@${digitalocean_droplet.controller-init.ipv4_address} cat /etc/rancher/k3s/k3s.yaml | sed -e 's|127.0.0.1:6443|${digitalocean_droplet.controller-init.ipv4_address}:6443|g' -e 's|/var/lib/rancher/k3s/server/tls/||g' | tee -a ${path.module}/cluster-config-k3s"
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl --kubeconfig=cluster-config-k3s get nodes"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm ${path.module}/cluster-config-k3s"
-  }
-
+data "external" "k3s_config" {
+  program = ["/bin/bash", "${path.module}/scripts/retrieve_kubeconfig.sh", "${digitalocean_droplet.controller-init.ipv4_address}", "${pathexpand(format("%s", local.ssh_key_name))}"]
 }
 
+resource "local_file" "cluster_k3s_config" {
+  content         = textdecodebase64(data.external.k3s_config.result.kubeconfig, "UTF-8")
+  filename        = pathexpand(format("%s-config", var.cluster_name))
+  file_permission = "0600"
+}
