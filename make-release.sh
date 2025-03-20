2eb77e62577150b04bee49fda3c0e1b7d7d97796
jmarhee <jmarhee@users.noreply.github.com>
2025-03-19 19:47:21 -0500

    Update make-release.sh
    Updates make-release.sh to correctly create a release from a tag.

--- make-release.sh
+++ make-release.sh
@@ -4,8 +4,15 @@ TAG=$TAG
 TAG_COMMIT=$(git rev-parse --short ${TAG})
 
 AUTH_TOKEN=$GITHUB_TOKEN
-curl \
-  -X POST "https://api.github.com/repos/jmarhee/terraform-digitalocean-ha-k3s/releases" -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\" }"
 
+curl -X POST \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/jmarhee/putio-cli-v2/releases" \
+  -d "{
+    \"tag_name\": \"$TAG\",
+    \"name\": \"Release $TAG\",
+    \"body\": \"Release notes for $TAG_COMMIT\",
+    \"draft\": false,
+    \"prerelease\": false
+  }"
